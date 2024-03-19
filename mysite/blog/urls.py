eaf85b2ce46a74ca00bf742bc9179e0578f7a7f6
samiryehia <samiryehia8@gmail.com>
2024-03-19 23:30:57 +0200

    setting up post urls

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -13,9 +13,10 @@ urlpatterns = [
     # An empty string as the first argument means this pattern matches the root URL for this app.
     # views.post_list is the view function that will be called if this URL pattern is matched.
     # The name='post_list' argument assigns a name to this URL pattern, allowing it to be referred to in templates and view functions.
-    path('allposts', views.post_list, name='post_list'),
+    path('', views.post_list, name='post_list'),
     #'/blog' is the prefix that is defined in the PROJECT urls.py file
     #'allposts' is the path that we define AFTER the prefix set in the urls PROJECT FILE.
     #So this is the path that we get at the end localhost:8000/blog/allposts.
+    path('post/<int:post_id>/', views.post_detail, name='post_detail'),
 
 ]
